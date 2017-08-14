9346342dc401d5706bc8ad7859c340302cbd7107
Loic Yvonnet <loic.yvonnet.pro@gmail.com>
2017-08-14 14:31:15 +0200

    Initial Commit

---
+++ 2d.convex.hull++/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2d.convex.hull++
+//
+//  Created by LOIC YVONNET on 14/08/2017.
+//  Copyright © 2017 LOIC YVONNET. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
