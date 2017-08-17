bd9d00bb7d8fba30250d0f73bd0b9c1d03fe390f
Loic Yvonnet <loic.yvonnet.pro@gmail.com>
2017-08-17 17:26:37 +0200

    Introduced monotone chain algorithm + unit tests

--- 2d.convex.hull++/algorithms.hpp
+++ 2d.convex.hull++/algorithms.hpp
@@ -25,7 +25,7 @@ namespace hull {
                 // swap points[1] with the point with the lowest y-coordinate
                 {
                     auto lowest_y = std::min_element(first, last, [](const auto& p1, const auto& p2) {
-                        return (y(p1) < y(p2) || (y(p1) == y(p2) && x(p1) < x(p2)));
+                        return (y(p1) < y(p2) || (hull::equals(y(p1), y(p2)) && x(p1) < x(p2)));
                     });
                     
                     std::swap(*first, *lowest_y);
@@ -103,12 +103,78 @@ namespace hull {
 
             return details::perform_graham_scan(first, last);
         }
+        
+        // Reference: https://en.wikibooks.org/wiki/Algorithm_Implementation/Geometry/Convex_hull/Monotone_chain
+        template <typename RandomIt1, typename RandomIt2>
+        RandomIt2 monotone_chain(RandomIt1 first, RandomIt1 last, RandomIt2 first2) {
+            using point_type1 = typename std::iterator_traits<RandomIt1>::value_type;
+            using point_type2 = typename std::iterator_traits<RandomIt2>::value_type;
+            
+            static_assert(is_point_v<point_type1>(), "ill-formed point");
+            static_assert(is_point_v<point_type2>(), "ill-formed point");
+            static_assert(std::is_same<
+                          typename std::iterator_traits<RandomIt1>::iterator_category,
+                          std::random_access_iterator_tag
+                          >(), "random access iterator required");
+            static_assert(std::is_same<
+                          typename std::iterator_traits<RandomIt2>::iterator_category,
+                          std::random_access_iterator_tag
+                          >(), "random access iterator required");
+            
+            // Sort the points of P by x-coordinate (in case of a tie, sort by y-coordinate)
+            std::sort(first, last, [](const auto& p1, const auto& p2) {
+                return (x(p1) < x(p2) || (hull::equals(x(p1), x(p2)) && y(p1) < y(p2)));
+            });
+            
+            const auto N = std::distance(first, last);
+            if (N <= 1) {
+                return std::copy(first, last, first2);
+            }
+            
+            std::size_t k{};
+            
+            auto no_counter_clockwise = [&k, first, first2](auto i) {
+                return cross(*(first2 + (k - 2)), *(first2 + (k - 1)), *(first + i)) <= 0;
+            };
+            
+            auto copy = [&k, first, first2](auto i) {
+                *(first2 + k) = *(first + i);
+                k++;
+            };
+            
+            // Lower hull
+            for (int i{}; i < N; i++) {
+                while (k >= 2 && no_counter_clockwise(i)) {
+                    k--;
+                }
+                copy(i);
+            }
+            
+            // Upper hull
+            auto t = k + 1;
+            for (int i{static_cast<int>(N - 2)}; i >= 0; i--) {
+                while (k >= t && no_counter_clockwise(i)) {
+                    k--;
+                }
+                copy(i);
+            }
+            
+            return first2 + (k - 1);
+        }
     }
     
     template <typename ForwardIt, typename RandomIt>
     auto compute_convex_hull(ForwardIt first, ForwardIt last, RandomIt first2) {
         auto last2 = std::copy(first, last, first2);
         return algorithms::graham_scan(first2, last2);
+    }
+    
+    template <typename RandomIt1, typename RandomIt2>
+    auto compute_convex_hull2(RandomIt1 first, RandomIt1 last, RandomIt2 first2) {
+        std::fill_n(first2, 2 * std::distance(first, last),
+                    std::declval<typename std::iterator_traits<RandomIt2>::value_type>());
+        
+        return algorithms::monotone_chain(first, last, first2);
     }
 }
 
---
+++ 2d.convex.hull++/monotone_chain_test.hpp
@@ -0,0 +1,294 @@
+#ifndef monotone_chain_test_h
+#define monotone_chain_test_h
+
+static auto test_monotone_chain = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 10>{{
+        {13, 5}, {12, 8}, {10, 3}, {7, 7},
+        {9, 6}, {4, 0}, {7, 1}, {7, 4},
+        {3, 3}, {1, 1}
+    }};
+    const auto expected = std::array<point2d, 6>{{
+        {1, 1}, {4, 0}, {7, 1},
+        {13, 5}, {12, 8}, {7, 7}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_with_negative_x = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 13>{{
+        {0, 10},
+        {-5, 5}, {-2, 5}, {2, 4}, {6, 5},
+        {-5, 1}, {-2, 3}, {1, 3}, {4, 2}, {7, 2},
+        {-3, 0}, {0, 0}, {3, 0}
+    }};
+    const auto expected = std::array<point2d, 7>{{
+        {-5, 1}, {-3, 0}, {3, 0}, {7, 2},
+        {6, 5}, {0, 10}, {-5, 5}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_general_case = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 23>{{
+        {5, 11}, {-3, 10}, {-6, -5}, {14, 11},
+        {-5, -14}, {-16, 0}, {2, -14}, {8, -8},
+        {-5, 0},  {5, 4}, {-10, 7}, {0, -6},
+        {-9, -8}, {17, -9}, {-16, -8}, {10, 8},
+        {2, -3}, {0, 14}, {-3, 4}, {11, 0},
+        {-12, -12}, {-5, 7}, {-14, -10}
+    }};
+    const auto expected = std::array<point2d, 9>{{
+        {-16, -8}, {-12, -12}, {-5, -14}, {2, -14},
+        {17, -9}, {14, 11}, {0, 14}, {-10, 7}, {-16, 0},
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_square_case = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 12>{{
+        {0, 0}, {5, 5}, {5, 0}, {-5, 0},
+        {-5, 5}, {-5, -5}, {0, -5}, {0, 5},
+        {5, -5}, {2, 3}, {-3, 2}, {-5, 4}
+    }};
+    const auto expected = std::array<point2d, 4>{{
+        {-5, -5}, {5, -5}, {5, 5}, {-5, 5}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_triangle_case = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 12>{{
+        {0, 0}, {5, -5}, {-5, -5}, {-1, 2},
+        {2, 0}, {-2, -1}, {1, 2}, {1, 1},
+        {2, -2}, {0, 5}, {-2, 1}, {1, 0}
+    }};
+    const auto expected = std::array<point2d, 3>{{
+        {-5, -5}, {5, -5}, {0, 5}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_with_empty_set = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 0>{{}};
+    const auto expected = std::array<point2d, 0>{{}};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_with_1_point = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 1>{{{0, 0}}};
+    const auto expected = std::array<point2d, 1>{{{0, 0}}};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_with_2_points = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 2>{{
+        {5, 0}, {-2, -3}
+    }};
+    const auto expected = std::array<point2d, 2>{{
+        {-2, -3}, {5, 0}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_with_3_points = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 3>{{
+        {5, 0}, {0, 0}, {-2, -3}
+    }};
+    const auto expected = std::array<point2d, 3>{{
+        {-2, -3}, {5, 0}, {0, 0}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_collinear_x = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 4>{{
+        {1, 1}, {-3, 1}, {-10, 1}, {10, 1}
+    }};
+    const auto expected = std::array<point2d, 2>{{
+        {-10, 1}, {10, 1}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_collinear_y = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 4>{{
+        {1, 1}, {1, -3}, {1, -10}, {1, 10}
+    }};
+    const auto expected = std::array<point2d, 2>{{
+        {1, -10}, {1, 10}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_collinear = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 4>{{
+        {23, 23}, {1, 1}, {3, 3}, {-7, -7}
+    }};
+    const auto expected = std::array<point2d, 2>{{
+        {-7, -7}, {23, 23}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_duplicate_origin = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 8>{{
+        {-6, -6}, {5, -4}, {-6, -6}, {7, 8},
+        {-6, -6}, {-6, -6}, {-6, -6}, {-6, -6}
+    }};
+    const auto expected = std::array<point2d, 3>{{
+        {-6, -6}, {5, -4}, {7, 8}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_duplicate_points = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 16>{{
+        {-5, 7}, {0, 0}, {5, -4}, {2, 3},
+        {-3, -2}, {-6, -6}, {7, 8}, {5, 6},
+        {7, 8}, {5, 6}, {-3, -2}, {-6, -6},
+        {5, -4}, {2, 3}, {-5, 7}, {0, 0}
+    }};
+    const auto expected = std::array<point2d, 4>{{
+        {-6, -6}, {5, -4}, {7, 8}, {-5, 7}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+static auto test_monotone_chain_more_duplicate_points = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 32>{{
+        {-5, 7}, {0, 0}, {5, -4}, {2, 3},
+        {-3, -2}, {-6, -6}, {7, 8}, {5, 6},
+        {7, 8}, {5, 6}, {-3, -2}, {-6, -6},
+        {5, -4}, {2, 3}, {-5, 7}, {0, 0},
+        {-5, 7}, {0, 0}, {5, -4}, {2, 3},
+        {-3, -2}, {-6, -6}, {7, 8}, {5, 6},
+        {7, 8}, {5, 6}, {-3, -2}, {-6, -6},
+        {5, -4}, {2, 3}, {-5, 7}, {0, 0}
+    }};
+    const auto expected = std::array<point2d, 4>{{
+        {-6, -6}, {5, -4}, {7, 8}, {-5, 7}
+    }};
+    std::vector<point2d> target(2 * points.size());
+    
+    // Act
+    const auto last = hull::algorithms::monotone_chain(std::begin(points), std::end(points), std::begin(target));
+    
+    // Assert
+    assert(std::distance(std::begin(target), last) == expected.size());
+    assert(std::equal(std::begin(target), last, std::begin(expected)));
+});
+
+#endif
