4acad3ac8def53d8ae20f4573dca9dbc976e4e84
Loic Yvonnet <loic.yvonnet.pro@gmail.com>
2017-08-22 13:01:53 +0200

    Added bounding_box algorithm

--- 2d.convex.hull++/algorithms_test.hpp
+++ 2d.convex.hull++/algorithms_test.hpp
@@ -165,4 +165,24 @@ static auto test_convex_hull_compute_with_container_with_monotone_chain = add_te
     assert(std::equal(std::begin(target), std::end(target), std::begin(expected)));
 });
 
+static auto test_bounding_box = add_test([] {
+    // Arrange
+    const auto points = std::array<point2d, 10>{{
+        {13, 5}, {-12, 8}, {10, 3}, {7, -7},
+        {-9, -6}, {4, 0}, {7, 1}, {7, 4},
+        {3, 3}, {-1, 1}
+    }};
+    const auto expected = std::array<point2d, 4>{{
+        {-12, -7}, {13, -7}, {13, 8}, {-12, 8}
+    }};
+    std::vector<point2d> target;
+    
+    // Act
+    hull::algorithms::bounding_box(std::begin(points), std::end(points), std::back_inserter(target));
+    
+    // Assert
+    assert(target.size() == expected.size());
+    assert(std::equal(std::begin(target), std::end(target), std::begin(expected)));
+});
+
 #endif
