63fd5f310738fc31cb223c8c2a60037630c38ffd
Loic Yvonnet <loic.yvonnet.pro@gmail.com>
2017-09-21 10:40:21 +0200

    Added unit test about m being too low for the Chan algorithm implementation

--- 2d.convex.hull++/test/chan_test.hpp
+++ 2d.convex.hull++/test/chan_test.hpp
@@ -26,4 +26,20 @@ static auto test_chan_impl = add_test([] {
     assert(std::equal(std::begin(target), std::end(target), std::begin(expected)));
 });
 
+static auto test_chan_impl_with_m_too_low = add_test([] {
+    // Arrange
+    auto points = std::array<point2d, 10>{{
+        {13, 5}, {12, 8}, {10, 3}, {7, 7},
+        {9, 6}, {4, 0}, {7, 1}, {7, 4},
+        {3, 3}, {1, 1}
+    }};
+    std::vector<point2d> target;
+    
+    // Act
+    const auto last = hull::algorithms::chan_impl(std::begin(points), std::end(points), std::back_inserter(target), 5);
+    
+    // Assert
+    assert(!last);
+});
+
 #endif
