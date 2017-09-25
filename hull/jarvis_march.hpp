01b4a0345464b130f7970f46ab6bc7df16a98b90
Loic Yvonnet <loic.yvonnet.pro@gmail.com>
2017-09-25 18:19:13 +0200

    Start refactoring Jarvis March's algorithm.

--- hull/jarvis_march.hpp
+++ hull/jarvis_march.hpp
@@ -12,6 +12,31 @@
 
 #include <algorithm>
 
+namespace hull::algorithms::details::jarvis {
+    /**
+     * Create a lambda which tells whether a given point
+     * sj is on the left of the vector p0p1.
+     * @param p0 - the first point of the vector.
+     * @param p1 - the second point of the vector.
+     * @return - a lambda function which takes a point sj
+     *           and returns true if this point is on the
+     *           left of the vector p0p1.
+     */
+    template <typename TPoint>
+    auto is_on_the_left(const TPoint& p0, const TPoint& p1) {
+        return [&p0, &p1](const auto& sj) {
+            const auto res = cross(p0, p1, sj);
+            
+            // If they are collinear, we take the farthest point from pi
+            if (hull::equals(res, static_cast<decltype(res)>(0))) {
+                return hull::square_norm(sj - p0) > hull::square_norm(p1 - p0);
+            }
+            
+            return res > 0;
+        };
+    }
+}
+
 namespace hull::algorithms::details {
     /**
      *
@@ -24,16 +49,7 @@ namespace hull::algorithms::details {
         auto endpoint = point_on_hull;
         
         // Helper lambda to determine wheter sj is on the left of line pi to endpoint
-        auto is_on_the_left = [&endpoint, &point_on_hull](const auto& sj) {
-            const auto res = cross(point_on_hull, endpoint, sj);
-            
-            // If they are collinear, we take the farthest point from pi
-            if (hull::equals(res, static_cast<decltype(res)>(0))) {
-                return hull::square_norm(sj - point_on_hull) > hull::square_norm(endpoint - point_on_hull);
-            }
-            
-            return res > 0;
-        };
+        auto is_on_the_left = jarvis::is_on_the_left(point_on_hull, endpoint);
         
         std::for_each(first, last, [&endpoint, &point_on_hull, is_on_the_left](const auto& sj) {
             // if (endpoint == pointOnHull) or (S[j] is on left of line from P[i] to endpoint)
@@ -78,40 +94,17 @@ namespace hull::algorithms {
         auto point_on_hull = *min_x;
         decltype(point_on_hull) endpoint;
         
-        // Helper lambda to determine wheter sj is on the left of line pi to endpoint
-        auto is_on_the_left = [&endpoint](const auto& pi, const auto& sj) {
-            const auto res = cross(pi, endpoint, sj);
-            
-            // If they are collinear, we take the farthest point from pi
-            if (hull::equals(res, static_cast<decltype(res)>(0))) {
-                return hull::square_norm(sj - pi) > hull::square_norm(endpoint - pi);
-            }
-            
-            return res > 0;
-        };
-        
         // Repeat until wrapped around to first hull point
         std::size_t i{};
         do {
             // P[i] = pointOnHull
             *(first2 + i) = point_on_hull;
             
-            // initial endpoint for a candidate edge on the hull
-            endpoint = *first2;
-            
-            // for j from 1 to |S|
-            std::for_each(first, last, [&endpoint, &point_on_hull, is_on_the_left](const auto& sj) {
-                // if (endpoint == pointOnHull) or (S[j] is on left of line from P[i] to endpoint)
-                if (hull::equals(endpoint, point_on_hull) || is_on_the_left(point_on_hull, sj)) {
-                    // found greater left turn, update endpoint
-                    endpoint = sj;
-                }
-            });
+            point_on_hull = details::max_jarvis_march(first, last, point_on_hull);
             
             i++;
-            point_on_hull = endpoint;
         }
-        while (!hull::equals(endpoint, *first2));
+        while (!hull::equals(point_on_hull, *first2));
         
         return first2 + i;
     }
